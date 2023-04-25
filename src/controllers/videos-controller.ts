0fed06cab558f5c254092351e141195a0077317f
alexanderbalaban <a.balaban@pixelplex.io>
2023-04-25 21:33:25 +0300

    add validate for /videos endpoints

--- src/controllers/videos-controller.ts
+++ src/controllers/videos-controller.ts
@@ -55,5 +55,4 @@ export const updateVideo = (req: Request, res: Response) => {
     } else {
         res.sendStatus(404);
     }
-
 }
--- src/middleware/validate-video-data.ts
+++ src/middleware/validate-video-data.ts
@@ -8,20 +8,33 @@ export const validateVideoData = (req: Request, res: Response, next: NextFunctio
     const {
         title,
         author,
-        availableResolutions
+        availableResolutions,
+        canBeDownloaded,
+        minAgeRestriction,
+        publicationDate
     } = req.body;
 
-    if (availableResolutions && availableResolutions.length) {
-
-        if (!availableResolutions.every((el: AvailableResolutions) => AvailableResolutions[el])) {
+    if (availableResolutions) {
+        if (!Array.isArray(availableResolutions)) {
             const error: ErrorField = {
-                message: `The field AvailableResolutions does not match the available resolutions`,
-                field: availableResolutions
+                message: `The field AvailableResolutions must be a array`,
+                field: "availableResolutions"
             };
             fieldsError.push(error);
         }
-    }
 
+        if (availableResolutions.length) {
+            if (!availableResolutions.every((el: AvailableResolutions) => AvailableResolutions[el])) {
+                const error: ErrorField = {
+                    message: `The field AvailableResolutions does not match the available resolutions`,
+                    field: "availableResolutions"
+                };
+                fieldsError.push(error);
+            }
+        }
+
+    }
+//--------------------------------------------------------------------------------------------------------
     if (!title) {
         const error: ErrorField = {
             message: `The field Title should is not equal undefined`,
@@ -29,15 +42,24 @@ export const validateVideoData = (req: Request, res: Response, next: NextFunctio
         };
         fieldsError.push(error);
     } else {
+
+        if (typeof title !== "string") {
+            const error: ErrorField = {
+                message: `The field Title must be a string`,
+                field: "title"
+            };
+            fieldsError.push(error);
+        }
+
         if (title.length >= 40) {
             const error: ErrorField = {
-                message: `The field Title must be a string with a maximum length of '40'`,
+                message: `The field Title must be a maximum length of '40'`,
                 field: "title"
             };
             fieldsError.push(error);
         }
     }
-
+//------------------------------------------------------------------------------------------------------
     if (!author) {
         const error: ErrorField = {
             message: `The field Author should is not equal undefined`,
@@ -45,10 +67,57 @@ export const validateVideoData = (req: Request, res: Response, next: NextFunctio
         };
         fieldsError.push(error);
     } else {
+
+        if (typeof author !== "string") {
+            const error: ErrorField = {
+                message: `The field Author must be a string`,
+                field: "author"
+            };
+            fieldsError.push(error);
+        }
+
         if (author.length >= 20) {
             const error: ErrorField = {
                 message: `The field Author must be a string with a maximum length of '20'`,
                 field: "author"
+            };
+            fieldsError.push(error);
+        }
+    }
+//-----------------------------------------------------------------------------------------------------
+    if (canBeDownloaded) {
+        if (typeof canBeDownloaded !== "boolean") {
+            const error: ErrorField = {
+                message: `The field CanBeDownloaded must be a boolean`,
+                field: "canBeDownloaded"
+            };
+            fieldsError.push(error);
+        }
+    }
+//-----------------------------------------------------------------------------------------------------
+    if (minAgeRestriction) {
+        if (typeof minAgeRestriction !== "number") {
+            const error: ErrorField = {
+                message: `The field minAgeRestriction must be a number`,
+                field: "minAgeRestriction"
+            };
+            fieldsError.push(error);
+        }
+
+        if (minAgeRestriction > 18 || minAgeRestriction < 1) {
+            const error: ErrorField = {
+                message: `The field minAgeRestriction must be maximum '18' and minimum '1'`,
+                field: "minAgeRestriction"
+            };
+            fieldsError.push(error);
+        }
+    }
+
+    if (publicationDate) {
+        if (typeof publicationDate !== "string") {
+            const error: ErrorField = {
+                message: `The field publicationDate must be a string`,
+                field: "publicationDate"
             };
             fieldsError.push(error);
         }
