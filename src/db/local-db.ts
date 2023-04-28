532708c37bdc2d8fe4c7a96a77fe0bfa90d2bad9
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-04-28 23:50:36 +0300

    test

--- src/db/local-db.ts
+++ src/db/local-db.ts
@@ -5,4 +5,4 @@ import {Video} from "../models/Video";
 export const DB: VideosType[] = [
     new Video("2 - Swagger", "Дмитрий Кузюбердин", [AvailableResolutions.P2160]),
     new Video("Express Router", "Дмитрий Кузюбердин", [AvailableResolutions.P2160])
-]
+];
